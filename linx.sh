953b478b0c21c6cc75c9e78fe1905fc4cda89761
hardgames <95406644+hardgs@users.noreply.github.com>
2022-05-26 12:21:57 +0000

    Add files via upload

--- linx.sh
+++ linx.sh
@@ -1,38 +1,85 @@
+#auther Group Hard Games 
+# Made Iran ( Group Hard Games"
+# We follow in the Github 
+# Link www.github.com/hardgs
+
+#Color list
+# Text
+Black="30"
+Red="31"
+Green="32"
+Yellow="33"
+Blue="34"
+Purple="35"
+Cyan="36"
+White="37"
+# BackGrand
+BBlack="40"
+BRed="41"
+BGreen="42"
+BYellow="43"
+BBlue="44"
+BPurple="45"
+BCyan="46"
+BWhite="47" 
+# Function  Write On Screen With Color
+DText(){
+        printf  "\033[${1};${2};${3}m ${4} \033[0m" 
+}
+SetText(){
+	    printf "\033[${1};${2};${3}m"
+}
+
+# Go To  Program
 #Include Or Init
+Dti(){
+SetText 0 1 $Red
+echo ""
+echo ""
+echo          "                                  :      :   "
+echo          ":                                  :    :    "
+echo          ":                                   :  :     "
+echo          ":          :::                        :      "
+echo          ":           :       :------          : :     "
+echo          ":           :       :      :        :   :    "
+echo          ":           :       :      :       :     :   "
+echo          ":           :       :      :      :       :  "
+echo          "---------   -       -      -     -         - "
+SetText 0 1 $Yellow
+}
+SetText 0 1 $Yellow
 clear
+Dti
 #function
 go(){
-    echo "[1]- IP :: [2]- YourMac :: [3]- YourIP"
-    echo "[4]- WRITE HTML ::  [5]- Exit :: "
+    printf "\n"
+    echo "[1]- Your Info || [2]- Site Info || [3]- Exit"
     #  Write On  screen
-    printf "[] : "
-    read inc
+    DText 0 1 $Red "[]- Chose One of top List : "
+    SetText 0 1 $Yellow
+    read En
     # Going 
-    case $inc in
-        1)
+    case $En in
+        2)
             printf "Site Or IP: "
             read si
-            echo "IP4: "$(ping $si -c 1 -4 | grep "64 bytes" | cut -d " " -f 5 | tr -d "(" | tr -d ")" | tr -d ":")
-            echo "IP6: "$(ping $si -c 1 -6 | grep "64 bytes" | cut -d " " -f 5 | tr -d "(" | tr -d ")" | tr -d ":")
-            ;;
-         2)
-            echo "Mac :"$(sudo ifconfig | grep "HWaddr" | cut -d " " -f 4)
-            ;;
-         3)
-            echo "IP : "$(sudo hostname -I | cut -d " " -f 1)
-            ;;
-         4)
             printf "Name :"
             read n
-            printf "Site :"
-            read s
             printf "Path :"
             read p
             touch "$p$n".html
-            curl $s > "$p$n".html
+            curl $si > "$p$n".html
             clear
+            Dti
+            echo "IP4: "$(ping $si -c 1 -4 | grep "64 bytes" | cut -d " " -f 5 | tr -d "(" | tr -d ")" | tr -d ":" || echo "No Site Or IP" || exit)
+            echo "IP6: "$(ping $si -c 1 -6 | grep "64 bytes" | cut -d " " -f 5 | tr -d "(" | tr -d ")" | tr -d ":" || echo "No Site Or IP" || exit)
+            echo "Default Document In the Addres: $p$n.html"
             ;;
-         5)
+         1)
+            echo "Mac :"$(sudo ifconfig | grep "HWaddr" | cut -d " " -f 4 || echo "Error Get Mac Address" || exit)
+            echo "IP : "$(sudo hostname -I | cut -d " " -f 1 || echo "Error Get Your IP" || exit)
+            ;;
+         3)
             exit
             ::
      esac
